019b1ee0a90652522a02238be78f740a1d5f65a2
ttktjmt <ta747375ki@gmail.com>
2024-03-17 11:42:16 +0100

    Add mkdocs
    Change frontend from React.js to mkdocs

--- frontend/docker-build-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run `docker login` beforehand when you're not logged in to the docker service
-
-DOCKERHUB_USERNAME="ttktjmt"
-IMAGE_NAME="ttktjmt-frontend"
-
-docker build -t $DOCKERHUB_USERNAME/$IMAGE_NAME .
-docker push $DOCKERHUB_USERNAME/$IMAGE_NAME
